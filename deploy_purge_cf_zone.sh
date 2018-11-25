92476549c550039b3d7e8fe83c1bcf228b440367
Chris Erickson <chris-erickson@users.noreply.github.com>
2018-11-25 15:17:17 -0600

    Add CF zone purging

---
+++ deploy_purge_cf_zone.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Purges the entire zone (all subdomains included)
+
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/"$CF_ZONE_ID"/purge_cache \
+  --header 'content-type: application/json' \
+  --header "x-auth-email: $CF_USER_EMAIL" \
+  --header "x-auth-key: $CF_USER_AUTH_KEY" \
+  --data '{"purge_everything": true}'
